5c6b9525e3ef118133d95129e54b49f8a0cf7620
Jaroslav Týc <mail@jaroslavtyc.com>
2018-05-30 18:27:42 +0200

    Convereted to new tests format

---
+++ DrdPlus/Tests/tests_config.php
@@ -0,0 +1,14 @@
+<?php
+global $testsConfiguration;
+$testsConfiguration = new \DrdPlus\Tests\RulesSkeleton\TestsConfiguration();
+$testsConfiguration->disableHasLinksToJournals();
+$testsConfiguration->disableHasLinkToSingleJournal();
+$testsConfiguration->disableHasCustomBodyContent();
+$testsConfiguration->disableHasProtectedAccess();
+$testsConfiguration->disableHasAuthors();
+$testsConfiguration->disableHasTables();
+$testsConfiguration->disableHasDebugContacts();
+$testsConfiguration->disableCanBeBoughtOnEshop();
+$testsConfiguration->disableHasCharacterSheet();
+$testsConfiguration->setBlockNamesToExpectedContent([]);
+$testsConfiguration->setBlockNamesToExpectedContent([]);
